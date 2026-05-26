7d35928c91426c9c47be660a8f01cc2cf578f3c2
atull31 <imatul5899@gmail.com>
2026-05-26 14:19:30 +0530

    Data Ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,19 +1,23 @@
+#this plays very important role
+#this ensures the automation
+#automation of importing raw data from 
+#various sources into a single system/storage
 import os
 import sys
-from src.exception import CustomException
-from src.logger import logging
+from src.exception import CustomException #calling exception we already customized it
+from src.logger import logging #same call for logger.py file
 import pandas as pd
-
 from sklearn.model_selection import train_test_split
-from dataclasses import dataclass
-
+from dataclasses import dataclass #important because \n:
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
-
 from src.components.model_trainer import ModelTrainerConfig
 from src.components.model_trainer import ModelTrainer
-@dataclass
+
+
+@dataclass #decorator for only defining variables 
 class DataIngestionConfig:
+    #inputs that we will be giving to data ingestion components
     train_data_path: str=os.path.join('artifacts',"train.csv")
     test_data_path: str=os.path.join('artifacts',"test.csv")
     raw_data_path: str=os.path.join('artifacts',"data.csv")
@@ -25,7 +29,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv('notebook\data\stud.csv')
+            df=pd.read_csv('notebook/data/stud.csv')
             logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
